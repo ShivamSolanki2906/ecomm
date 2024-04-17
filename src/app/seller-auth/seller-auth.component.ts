00e2b0bb9b334350258d77703c7e58c1cb32f2ba
Shivam Solanki <rajputshivam2906@.in>
2024-04-17 11:41:16 +0530

    routing and json-server create api

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.scss']
+})
+export class SellerAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {} 
+
+  signUpForm(data:object):void{
+    console.log(data);
+    
+  }
+
+}
