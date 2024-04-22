dd5d561883ce5341cd82fe264f1f8c3da63c72c2
Shivam Solanki <rajputshivam2906@.in>
2024-04-22 11:47:26 +0530

    update navber

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  menuType:String = "default"
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe((val:any)=>{
+        // console.log(val.url); 
+        if(val.url){
+          console.log(val.url);
+          if(localStorage.getItem('seller') && val.url.includes('seller')){
+            this.menuType ="seller"
+            console.log("seller area");   
+          }else{
+            console.log("not seller area");
+            this.menuType= "default"
+            
+          }
+        } 
+    })
   }
 
 }
